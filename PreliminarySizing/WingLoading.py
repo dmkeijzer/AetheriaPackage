8bf753275b70c90d8d05cfeee24e120964fc9949
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-08 16:54:58 +0200

    small change

--- PreliminarySizing/WingLoading.py
+++ PreliminarySizing/WingLoading.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+''' 
+Wing sizing is mainly done the same as for normal fixed-wing(FW) aircraft
+since they are not used during the take-off and landing phase but are used
+during cruise. The stall speed is chosen as 30 m/s as ...
+''' 
 import numpy as np
 
-def TW_to_PW(TW,V,eff_prop):
+def tw_to_pw(TW,V,eff_prop):
     return TW*V/eff_prop
 
 WS_stall = 0.5*V_stall*rho0*CLMax
