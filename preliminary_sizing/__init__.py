c2314df690e1df4a5782db1b02634957e725506e
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-08 17:13:46 +0200

    test

--- preliminary_sizing/__init__.py
+++ preliminary_sizing/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from powerloading import WigeonPowerLoading
+from wingloading import wingloading_stall
+
--- preliminary_sizing/wingloading.py
+++ preliminary_sizing/wingloading.py
@@ -7,4 +7,5 @@ h_transition = 20 #m
 t_TO = 20
 acc_v = 2*h_transition/(t_TO*t_TO)
 
-TW = acc_v/g - 1
+def wingloading_stall(CLmax,V_stall,rho):
+    WS = CLmax*0.5*rho*V_stall*V_stall
