09058a00d68c55c65e1fe4f1ec11e777a3eb2c3d
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-10 11:20:42 +0200

    added powerfunctions

--- preliminary_sizing/__init__.py
+++ preliminary_sizing/__init__.py
@@ -2,4 +2,8 @@
 
 from powerloading import WigeonPowerLoading
 from wingloading import wingloading_stall
-
+from powerloading import powerloading_climbrate
+from powerloading import powerloading_turningloadfactor
+from powerloading import powerloading_thrustloading
+from powerloading import powerloading_verticalflight_ducted
+from powerloading import powerloading_verticalflight_open
