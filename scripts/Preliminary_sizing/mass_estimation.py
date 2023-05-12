9dcc0c4a1ff772d9000f89e0a536215b5acd2c04
jorickarends <jorjel1501@gmail.com>
2023-05-12 16:30:09 +0200

    Deleted function in massestimation that runs always (is annoying)

--- scripts/Preliminary_sizing/mass_estimation.py
+++ scripts/Preliminary_sizing/mass_estimation.py
@@ -4,11 +4,11 @@ import sys
 import os
 import pathlib as pl
 
-
-
 sys.path.append(str(list(pl.Path(__file__).parents)[2]))
 
 from modules.preliminary_sizing import *
+
+
 
 Structure.MTOMclassI, Structure.OEMclassI = mass_estimation(Structure.payload, PLOT=True, PRINT=True)
 
