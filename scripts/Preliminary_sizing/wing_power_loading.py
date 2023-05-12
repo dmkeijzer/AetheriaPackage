afe35c7074f07457a359ec050d1fef9595ccd26e
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-12 10:36:58 +0200

    updated and finished WS_WP diagrams

--- scripts/Preliminary_sizing/wing_power_loading.py
+++ scripts/Preliminary_sizing/wing_power_loading.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 from modules.preliminary_sizing import *
 
 
-WS_range = np.arange(0,4000,1)
-ylim = [0,0.25]
+WS_range = np.arange(1,4000,1)
+ylim = [0,0.15]
 
 
 def plot_wing_power_loading_graphs(eff, StotS, diskloading, no_engines,name,WS_range,i):
@@ -32,20 +32,43 @@ def plot_wing_power_loading_graphs(eff, StotS, diskloading, no_engines,name,WS_r
     VERTICALFLIGHT = powerloading_verticalflight(TW_range,diskloading,rho0,eff,ducted_bool)
     STALLSPEED = wingloading_stall(Aero.CLmax,Performance.V_stall, rho0)
     
+    #Plot all the lines
     plt.plot(WS_range,CLIMBRATE,label="Climbrate")
     plt.plot(WS_range,TURN_VCRUISE,label='Turnload@cruise speed')
     plt.plot(WS_range,TURN_VMAX,label='Turnload@max speed')
     plt.plot(WS_range,VERTICALFLIGHT,label='Vertical flight/TO')
     plt.vlines(STALLSPEED,ymin=ylim[0],ymax=ylim[1],label='Stall speed:CLmax=1.5')
 
+    #DETERMINE LOWEST
+    lowest_area_y_novf = []
+    lowest_area_y = []
+    lowest_area_x = np.arange(0,int(STALLSPEED),1)
+    for i in lowest_area_x:
+        lowest_area_y.append(min(CLIMBRATE[i],TURN_VCRUISE[i],TURN_VMAX[i],VERTICALFLIGHT[i]))
+        lowest_area_y_novf.append(min(CLIMBRATE[i],TURN_VCRUISE[i],TURN_VMAX[i]))
+    #cover interesting ares.
+    plt.fill_between(lowest_area_x,lowest_area_y, color = 'Green', alpha = 0.3)
+    plt.fill_between(lowest_area_x,lowest_area_y_novf, color = 'Green', alpha = 0.2)
     
+    #Plot top right design points
+    if lowest_area_y_novf[-1] == lowest_area_y[-1]:
+        plt.plot(STALLSPEED,lowest_area_y_novf[-1],marker = 'o',color = 'green')
+        plt.annotate((str(int(STALLSPEED))+', '+str(round(lowest_area_y_novf[-1],8))),(STALLSPEED,lowest_area_y_novf[-1]+0.005))
+    else: 
+        plt.plot(STALLSPEED,lowest_area_y_novf[-1],marker = 'o',color = 'green')
+        plt.annotate((str(int(STALLSPEED))+', '+str(round(lowest_area_y_novf[-1],8))),(STALLSPEED,lowest_area_y_novf[-1]+0.005))
+        plt.plot(STALLSPEED,lowest_area_y[-1],marker = 'o',color = 'red')
+        plt.annotate((str(int(STALLSPEED))+', '+str(round(lowest_area_y[-1],8))),(STALLSPEED,lowest_area_y[-1]-0.01))
+    #Graph make-up
     plt.legend()
-    #plt.grid()
     plt.xlabel('Wingloading W/S')
     plt.ylabel('Powerloading W/P')
     plt.xlim([WS_range[100],WS_range[-1]])
     plt.ylim(ylim)
-    # plt.savefig('input_output/wing_power_loading_diagrams/'+str(name))
+    output_directory = str(list(pl.Path(__file__).parents)[2])+'\\output\\wing_power_loading_diagrams\\'
+    plt.savefig(output_directory+str(name)+'.png')
+    #print(os.path.dirname(os.getcwd()))
+    
 
 #FIRST EASY PRELIMINARY DESIGN
 name = ['Joby-like',  'Lilium-like','Wigeon-like']
