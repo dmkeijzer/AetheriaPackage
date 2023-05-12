69a42d95e606772d0d5825c1a72e0d5b5f50a990
jorickarends <jorjel1501@gmail.com>
2023-05-12 13:41:26 +0200

    Added figures of mass estimation to output directory

--- Modules/preliminary_sizing/__init__.py
+++ Modules/preliminary_sizing/__init__.py
@@ -7,4 +7,4 @@ sys.path.append(str(list(pl.Path(__file__).parents)[2]))
 from input.GeneralConstants import *
 from .powerloading import *
 from .wingloading import *
-#from .massestimation import *
+from .massestimation import *
