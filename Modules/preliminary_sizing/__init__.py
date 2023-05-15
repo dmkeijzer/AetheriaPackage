e9355ee986faf08bf2aad03c2d7351e83437228d
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-15 10:14:40 +0200

    changed __init__ for preliminary sizing

--- Modules/preliminary_sizing/__init__.py
+++ Modules/preliminary_sizing/__init__.py
@@ -5,6 +5,6 @@ import pathlib as pl
 sys.path.append(str(list(pl.Path(__file__).parents)[2]))
 
 from input.GeneralConstants import *
-from .powerloading import *
-from .wingloading import *
-from .massestimation import *
+from modules.preliminary_sizing.powerloading import *
+from modules.preliminary_sizing.wingloading import *
+from modules.preliminary_sizing.massestimation import *
