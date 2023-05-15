f3871d9761b3968d58ed8bee3407e95de145b55a
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-15 17:31:48 +0200

    Finished wingloading

--- Modules/preliminary_sizing/powerloading.py
+++ Modules/preliminary_sizing/powerloading.py
@@ -16,12 +16,17 @@ def powerloading_turningloadfactor(rho,V,WS,eff,A,e,loadfactor,CD0):
 
 def powerloading_thrustloading(WS,rho,ROC,StotS):
     return 1.2*(1+(1/WS)*rho*ROC**2*StotS)
+    #return 1.2*(1+np.ones(np.shape(WS)))*1.3
 
-def powerloading_verticalflight(TW,diskloading,rho,eff,ducted_bool):
+def powerloading_verticalflight(MTOM,TW,A_tot,rho,eff,ducted_bool,g):
+    W = MTOM *9.81
+    T = TW * W
+    
     if ducted_bool==True:
-        return (0.5*TW*np.sqrt(diskloading/rho))**(-1)*eff
+        return (0.5*np.sqrt((T*T*T)/(W*W*rho*A_tot)))**(-1)*eff
     else:
-        return (TW*np.sqrt(diskloading/(2*rho)))**(-1)*eff
+        return (np.sqrt((T*T*T)/(2*W*W*rho*A_tot)))**(-1)*eff    
+       
 def powerloading_climbgradient(e,A,CD0,WS,rho,eff,G):
     CL = np.sqrt(np.pi*e*A*CD0)
     CD = 2*CD0
