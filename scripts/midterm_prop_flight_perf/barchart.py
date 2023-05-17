ecf107f2a0ee3ebfd46784470750456c2b227fa1
lucasmiddendorp <75258151+lucasmiddendorp@users.noreply.github.com>
2023-05-17 16:08:00 +0200

    update barchart

--- scripts/midterm_prop_flight_perf/barchart.py
+++ scripts/midterm_prop_flight_perf/barchart.py
@@ -11,8 +11,10 @@ dict_directory = "input"
 dict_names = ["J1_constants.json", "L1_constants.json", "W1_constants.json"]
 download_dir = os.path.join(os.path.expanduser("~"), "Downloads")
 
+bardata = []
 # Loop through the JSON files
 for dict_name in dict_names:
+    
     # Load data from JSON file
     with open(os.path.join(dict_directory, dict_name)) as jsonFile:
         data = json.load(jsonFile)
@@ -20,6 +22,28 @@ for dict_name in dict_names:
     mission = [data["takeoff_energy"], data["trans2hor_energy"], data["climb_energy"],  
                data["cruise_energy"], data["descend_energy"], data["hor_loiter_energy"],  
                data["trans2ver_energy"], data["ver_loiter_energy"], data["land_energy"]]
-    labels = ["Take-off", "Transition to horizontal", "Climbing", "Cruise", "Descend", "Loiter horizontal","Transition to vertical", "Loiter vertical", "Landing"]
-    plt.pie(mission, labels=labels, colors=colors, autopct='%1.1f%%')
-    plt.show()
+    bardata.append(mission)
+    print(bardata)
+
+labels = ["Take-off", "Transition to horizontal", "Climbing", "Cruise", "Descend", "Loiter horizontal","Transition to vertical", "Loiter vertical", "Landing"]
+
+# Width of each bar
+bar_width = 0.1
+
+# X locations for the bars
+x = np.arange(len(labels))
+
+# Plotting the bar graph
+for i, segments in enumerate(bardata):
+    plt.bar(x + (i * bar_width), segments, width=bar_width, tick_label= labels)
+
+plt.xlabel('Mission Segments')
+plt.xticks(rotation= -45, fontsize= 8)
+plt.ylabel('Energy (J)')
+
+name = ["J1", "L1", "W1"]
+plt.legend(labels=name, loc="upper right",fontsize=6)
+plt.title("Mission Energy "+ data["name"], fontsize = 12)
+plt.tight_layout()
+
+plt.show()
