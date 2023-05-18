41c3202dfc3e4a041c09782e6fbe952310bc352e
jorickarends <jorjel1501@gmail.com>
2023-05-18 15:49:07 +0200

    Improved nvm.py and nvm_script.py

--- modules/midterm_structures/__init__.py
+++ modules/midterm_structures/__init__.py
@@ -6,4 +6,4 @@ import pathlib as pl
 sys.path.append(str(list(pl.Path(__file__).parents)[2]))
 
 from input.GeneralConstants import *
-from modules.midterm_structures.structural_forms import *
+#from modules.midterm_structures.structural_forms import *
