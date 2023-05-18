b85cf8b7a4becf1c7d4211f094a0bbaa16080ebe
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-19 00:14:46 +0200

    updated loadcases

--- modules/midterm_structures/structural_forms.py
+++ modules/midterm_structures/structural_forms.py
@@ -20,7 +20,6 @@ def j_z_thinwalled(width,height,thickness):
     return (height+width)*height*width*thickness/3
 
 
-
 """NORMAL STRESS"""
 def bending_stress(moment_x,moment_y,i_xx,i_yy,i_xy,x,y):
     return((moment_x*i_yy-moment_y*i_xy)*y + (moment_y*i_xx - moment_x*i_xy)*x)/(i_xx*i_yy-i_xy*i_xy)
