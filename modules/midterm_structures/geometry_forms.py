70c57229e770998650fb0d152b203eca11e9312e
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-05-20 03:47:16 +0200

    Finished the stress calculations

--- modules/midterm_structures/geometry_forms.py
+++ modules/midterm_structures/geometry_forms.py
@@ -8,22 +8,25 @@ import numpy as np
 def i_xx_solid(width, height):
     return width * height * height * height / 12
 
-def i_yy_solid(width, height):
+def i_zz_solid(width, height):
     return width * width * width * height / 12
 
-def j_z_solid(width, height):
+def j_y_solid(width, height):
     return width * height * (width * width + height * height) / 12
 
 def i_xx_thinwalled(width, height, thickness):
     return 1 / 3 * width * height * height * thickness
 
-def i_yy_thinwalled(width, height, thickness):
+def i_zz_thinwalled(width, height, thickness):
     return 1 / 3 * width * width * height * thickness
 
-def j_z_thinwalled(width, height, thickness):
+def j_y_thinwalled(width, height, thickness):
     return (height + width) * height * width * thickness / 3
 
 
 
 def enclosed_area_thinwalled(width,height,thickness):
     return (height-thickness)*(width-thickness)
+
+def area_thinwalled(width,height,thickness):
+    return (width*height - (width-thickness*2)*(height - thickness*2))
