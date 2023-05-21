91c2a4d5f35ba945962d4a29c9053feb56b7e537
csimonsoria <100701122+carmelosimon@users.noreply.github.com>
2023-05-21 18:25:36 +0200

    Fixed c2/c1 in envelope plotter, fixed c2 in W1 inputs

--- temp_stab_and_ctrl/cg_envelope_plotter.m
+++ temp_stab_and_ctrl/cg_envelope_plotter.m
@@ -98,7 +98,7 @@ table_data = readtable(file_path);
 
 
 x_np_nom=-Cma1+(CLa1*x1)/(c1)-Cma2*(1-downwash)*(S2/S1)*(c2/c1)*(V2_V1_ratio)^2+CLa2*(1-downwash)*(x2/c1)*(S2/S1)*(V2_V1_ratio)^2;
-x_np_den=CLa1/c1+CLa2/c1*(1-downwash)*(S2/S1)*(V2_V1_ratio)^2*(c2/c1);
+x_np_den=CLa1/c1+CLa2/c1*(1-downwash)*(S2/S1)*(V2_V1_ratio)^2;
 x_np=x_np_nom/x_np_den;
 
 x_cg = (-Cm1-Cm2*(c2/c1)*(S2/S1)*V2_V1_ratio^2+CL1*x1/c1+CL2*(x2/c1)*(S2/S1)*V2_V1_ratio^2)/(CL1/c1 + (CL2/c1)*(S2/S1)*V2_V1_ratio^2);
