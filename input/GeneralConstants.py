dff3de2b7d86a6d86a03aebb16a58e0eda97ebc9
lucasmiddendorp <75258151+lucasmiddendorp@users.noreply.github.com>
2023-05-31 16:18:10 +0200

    change airfoil to naca2412

--- input/GeneralConstants.py
+++ input/GeneralConstants.py
@@ -66,8 +66,8 @@ m_pl =  510  # kg
 
 
 #airfoil
-toc = 0.169 #NACA44017
-xcm = 0.293 # NACA44017
+toc = 0.12 #NACA2412
+xcm = 0.3 # NACA2412 position of maximum thickness
 A_base = 0 #Assumed 0 base area
 frac_lam_fus = 0.05
 frac_lam_wing = 0.1
