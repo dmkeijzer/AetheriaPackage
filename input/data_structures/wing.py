b4c75c3d35a884bc3d4f3a2a2a216d6a7ae4350d
csimonsoria <100701122+carmelosimon@users.noreply.github.com>
2023-06-01 15:06:34 +0200

    final update wing_loc_horzstab_sizing

--- input/data_structures/wing.py
+++ input/data_structures/wing.py
@@ -34,6 +34,7 @@ class Wing():
     cL_approach: float = None
     cL_alpha0: float = None
     mach_cruise: float = None
+    cL_alpha0_approach: float = None
 
     def load(self):
         with open(r"input/data_structures/aetheria_constants.json") as jsonFile:
@@ -60,6 +61,7 @@ class Wing():
         self.cL_approach = data["cL_approach"]
         self.cL_alpha0 = data["cL0"]
         self.mach_cruise = data["mach_cruise"]
+        self.cL_alpha0_approach = data["cL0_approach"]
         # self.effective_aspectratio =  data[""] # Left out for now since it is not implemented yet
         # self.effective_span =  data[""] # Left out for now since it is not implemented yet
     
