1280e750c8bb146439f91615bf82e0862d759b42
Damien Keijzer <damien.keijzer@gmail.com>
2023-06-02 14:07:55 +0200

    something for wingbox sizing

--- scripts/structures/wingbox_sizing.py
+++ scripts/structures/wingbox_sizing.py
@@ -3,6 +3,7 @@ import os
 import pathlib as pl
 from scipy.optimize import minimize
 import numpy as np
+import pickle
 
 sys.path.append(str(list(pl.Path(__file__).parents)[2]))
 
@@ -45,6 +46,8 @@ x0=np.array([7, 1.5, 0.003, 0.003, 0.12, 0.07, 0.003,0.003,0.004,0.0022])
 
 
 res = wingbox_optimization(x0, MatClass, WingClass, EngClass)
+
+with open()
 
 
 print(res)
