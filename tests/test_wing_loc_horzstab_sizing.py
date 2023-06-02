3ff0bc429789cb5154d181baf9ce03c5691293b2
csimonsoria <100701122+carmelosimon@users.noreply.github.com>
2023-06-02 11:58:50 +0200

    Finished pytest for wing_loc_horzstab_sizing

--- tests/test_wing_loc_horzstab_sizing.py
+++ tests/test_wing_loc_horzstab_sizing.py
@@ -11,17 +11,17 @@ from scripts.stab_ctrl.wing_loc_horzstab_sizing import stabcg, ctrlcg, CLaAhcalc
 
 def example_values():
     return {
-        "stabcg": {"ShS": None, "x_ac": None, "CLah": None, "CLaAh": None, "depsda": None, "lh": None, "c": None, "VhV2": None},
-        "ctrlcg": {"ShS": None, "x_ac": None, "Cmac": None, "CLAh": None, "CLh": None,"lh": None, "c": None, "VhV2": None},
-        "CLaAh": {"CLaw": None, "b_f": None, "b": None, "S": None, "c_root": None},
-        "x_ac_fus1": {"b_f": None, "h_f": None, "l_fn": None, "CLaAh": None, "S": None, "MAC": None},
-        "x_ac_fus2": {"b_f": None, "S": None, "b": None, "Lambdac4": None, "taper": None, "MAC": None},
-        "beta": {"M": None},
-        "CLah": {"A_h": None, "beta": None, "eta": None, "Lambdah2": None},
-        "stabformula": {"CLah": None, "CLaAh": None, "depsda": None, "l_h": None, "MAC": None, "Vh_V_2": None, "x_ac_stab_bar": None, "SM": None},
-        "CLh": {"A_h", None},
-        "cmac_fus": {"b_f": None, "l_f": None, "h_f": None, "CL0_approach": None, "S": None, "MAC": None, "CLaAh": None},
-        "ctrlformula": {"CLh_approach": None, "CLAh_approach": None, "l_h": None, "MAC": None, "Vh_V_2": None, "Cm_ac": None, "x_ac_stab_bar": None},
+        "stabcg": {"ShS": 0.15, "x_ac": 0.24, "CLah": 1.6, "CLaAh":3.7, "depsda": 0.11, "lh": 3, "c": 1.2, "VhV2": 0.95, "SM": 0.05},
+        "ctrlcg": {"ShS": 0.15, "x_ac": 0.24, "Cmac": -0.4, "CLAh": 1.7, "CLh": -0.3,"lh": 3, "c": 1.2, "VhV2": 0.95},
+        "CLaAh": {"CLaw": 3, "b_f": 0.9, "b": 10, "S": 12, "c_root": 1.2},
+        "x_ac_fus1": {"b_f": 0.9, "h_f": 1.1, "l_fn": 2.8, "CLaAh":3.7, "S":12, "MAC":1.2},
+        "x_ac_fus2": {"b_f": 0.9, "S":12, "b": 10, "Lambdac4": np.radians(2), "taper":0.4, "MAC":1.2},
+        "beta": {"M": 0.23},
+        "CLah": {"A_h": 5, "beta": 0.92, "eta": 0.95, "Lambdah2":np.radians(15)},
+        "stabformula": {"CLah": 1.6, "CLaAh": 3.7, "depsda":0.11, "l_h":3, "MAC": 1.2, "Vh_V_2":0.95, "x_ac_stab_bar":0.24, "SM": 0.05},
+        "CLh": {"A_h", 5},
+        "cmac_fus": {"b_f": 0.9, "l_f": 7, "h_f":1.1, "CL0_approach":0.6, "S":12, "MAC":1.2, "CLaAh":3.7},
+        "ctrlformula": {"CLh_approach": -0.5, "CLAh_approach": 1.9, "l_h":3, "MAC":1.2, "Vh_V_2": 0.95, "Cm_ac":-0.4, "x_ac_stab_bar": 0.24},
     }
 def test_wing_loc_horzstab_sizing(example_values):
     stabcgtest = stabcg(**example_values["stabcg"])
@@ -36,16 +36,16 @@ def test_wing_loc_horzstab_sizing(example_values):
     cmac_fus = cmac_fuselage_contr(**example_values["cmac_fus"])
     ctrlm, ctrlq = ctrl_formula_coefs(**example_values["ctrlformula"])
 
-    assert np.isclose(stabcgtest, 0)
-    assert np.isclose(ctrlcgtest, 0)
-    assert np.isclose(CLaAh, 0)
-    assert np.isclose(x_ac_fus_1, 0)
-    assert np.isclose(x_ac_fus_2, 0)
-    assert np.isclose(beta, 0)
-    assert np.isclose(CLah, 0)
-    assert np.isclose(stabm, 0)
-    assert np.isclose(stabq, 0)
-    assert np.isclose(CLh, 0)
-    assert np.isclose(cmac_fus, 0)
-    assert np.isclose(ctrlm, 0)
-    assert np.isclose(ctrlq, 0)
+    assert np.isclose(stabcgtest, 0.327108108)
+    assert np.isclose(ctrlcgtest, 0.412426471)
+    assert np.isclose(CLaAh, 3.36428375)
+    assert np.isclose(x_ac_fus_1, -0.0936486486)
+    assert np.isclose(x_ac_fus_2, 0.000324502019)
+    assert np.isclose(beta, 0.9471)
+    assert np.isclose(CLah, 4.23088125)
+    assert np.isclose(stabm, 1.0940272)
+    assert np.isclose(stabq, 0.207865168)
+    assert np.isclose(CLh, -0.598491581)
+    assert np.isclose(cmac_fus, -0.0748648959)
+    assert np.isclose(ctrlm, -1.6)
+    assert np.isclose(ctrlq, 0.720842105)
