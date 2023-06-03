1efff2cf8355e1c4b8bd9fa8b50dbeb9cbb579c8
Damien Keijzer <damien.keijzer@gmail.com>
2023-06-03 18:41:34 +0200

    Tried some other stuff, minimize function is simply borken honestly

--- scripts/structures/wingbox_sizing.py
+++ scripts/structures/wingbox_sizing.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import pathlib as pl
-from scipy.optimize import minimize
+from scipy.optimize import minimize, LinearConstraint, Bounds
 import numpy as np
 import pickle
 
@@ -44,10 +44,21 @@ wb.n_max= const.n_max_req
 
 #------------------------------- Run script and save----------------------------------------------
 
-x0=np.array([WingClass.span, WingClass.chord_root, 0.003, 0.003, 0.12, 0.07, 0.003,0.003,0.004,0.0022])    # :param x0: Initial estimate Design vector X = [b, cr, tsp, trib, L, bst, hst, tst, wst, t]
+x0=np.array([WingClass.span, WingClass.chord_root, 0.003, 0.002, 0.01, 0.01, 0.010,0.003,0.004,0.0022])    # :param x0: Initial estimate Design vector X = [b, cr, tsp, trib, L, bst, hst, tst, wst, t]
 # bnds = wb.create_bounds(WingClass) # create bounds
-bnds = ((4, 9), (1, 4), (0.001, 0.005), (0.001, 0.005), (0.007, 0.05), (0.001, 0.01),(0.001, 0.01),(0.001, 0.003),(0.004, 0.005),(0.001, 0.003))
-res = wb.wingbox_optimization(x0, bnds)
+# bnds = ((8, 12), (1, 4), (0.001, 0.009), (0.001, 0.003), (0.003, 0.015), (0.001, 0.01),(0.001, 0.01),(0.001, 0.003),(0.004, 0.005),(0.001, 0.003))
+
+# This will not work:
+#bounds = Bounds([-2.0,-0.5,-2.0],[2.0,0.8,0.7])
+
+# This works
+lb = [8, 1, 0.001, 0.001, 0.003, 0.001, 0.001, 0.001, 0.002, 0.001]
+ub = [12, 4, 0.009, 0.003, 0.015, 0.02, 0.10, 0.004, 0.005, 0.003]
+# A = [list(i) for i in np.eye(10)]
+# lcons = LinearConstraint(A, lb=lb, ub=ub, keep_feasible=True)
+bnds = Bounds(lb, ub, keep_feasible= True)
+# Res = minimize(RosenbrockN, x0, method = 'trust-constr', constraints=lcons) 
+res = wb.wingbox_optimization(x0, bnds, WingClass)
 
 with open(r"output/structures/wingbox_output.pkl", "wb") as f:
     pickle.dump(res, f)
