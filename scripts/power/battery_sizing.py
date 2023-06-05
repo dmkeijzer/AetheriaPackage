4379fb456e917c057bf46254d9f1e840235ad714
ehadzhiyski <127728776+ehadzhiyski@users.noreply.github.com>
2023-06-05 14:26:07 +0200

    ..

--- scripts/power/battery_sizing.py
+++ scripts/power/battery_sizing.py
@@ -6,10 +6,12 @@ import pathlib as pl
 #os.chdir(str(list(pl.Path(__file__).parents)[2]))
 
 from input.data_structures import Battery
-bat = Battery(EnergyDensity=0.34, PowerDensity=3.8, VolumeDensity=0.85, Depth_of_discharge=1, CostDensity=100, End_of_life_cycle=0.8,Energy=34, Power=500)
+bat = Battery(EnergyDensity=0.34, PowerDensity=3.8, VolumeDensity=0.85, Depth_of_discharge=1, CostDensity=100, End_of_life_cycle=0.8, Energy=34, Power=500)
 
+# def battery_mass_energy():
+#     return bat
 
-def f(battery : Battery):
-    specificE = battery.EnergyDensity
+#def f(battery : Battery):
+    #specificE = battery.EnergyDensity
 
-Battery.energymass()
+print(Battery.energymass())
