b1dc5e9447855d0e966b63d2ed557e638c4aa0e8
Damien Keijzer <damien.keijzer@gmail.com>
2023-06-06 16:02:37 +0200

    This is the version where Torsion was still taken into account if we want to go back to this

--- modules/structures/wingbox_georgina.py
+++ modules/structures/wingbox_georgina.py
@@ -609,102 +609,100 @@ def N_xy(b, c_r, t_sp, t_rib, L, b_st, h_st,t_st,w_st,t,Engine,Wing):
         Nxy[i] = determine
     return Nxy
 
-'''
-def N_xy(b, c_r, t_sp, t_rib, L, b_st, h_st,t_st,w_st,t):
-    h1 = height(b, c_r)
-    ch = chord(b, c_r)
-    tarr = t_arr(b,L,t)
-    sta = rib_coordinates(b, L)
-    Vz=shear_force(b, c_r, t_sp, t_rib, L, b_st, h_st,t_st,w_st,t)
-    Nxy = np.zeros(len(tarr))
+# def N_xy(b, c_r, t_sp, t_rib, L, b_st, h_st,t_st,w_st,t):
+#     h1 = height(b, c_r)
+#     ch = chord(b, c_r)
+#     tarr = t_arr(b,L,t)
+#     sta = rib_coordinates(b, L)
+#     Vz=shear_force(b, c_r, t_sp, t_rib, L, b_st, h_st,t_st,w_st,t)
+#     Nxy = np.zeros(len(tarr))
 
-    for i in range(len(tarr)):
-        Ixx1 = I_xx(b,c_r,t_sp,b_st, h_st,t_st,w_st,tarr[i])
-        Ixx = Ixx1(sta[i])
-        h = h1(sta[i])
-        l_sk = sqrt(h ** 2 + (0.25 * c_r) ** 2)
-        c = ch(sta[i])
+#     for i in range(len(tarr)):
+#         Ixx1 = I_xx(b,c_r,t_sp,b_st, h_st,t_st,w_st,tarr[i])
+#         Ixx = Ixx1(sta[i])
+#         h = h1(sta[i])
+#         l_sk = sqrt(h ** 2 + (0.25 * c_r) ** 2)
+#         c = ch(sta[i])
 
-        # Base region 1
-        qb1 = lambda z: Vz[i] * tarr[i] * (0.5 * h) ** 2 * (np.cos(z) - 1) / Ixx
-        I1 = qb1(pi / 2)
+#         # Base region 1
+#         qb1 = lambda z: Vz[i] * tarr[i] * (0.5 * h) ** 2 * (np.cos(z) - 1) / Ixx
+#         I1 = qb1(pi / 2)
 
-        # Base region 2
-        qb2 = lambda z: -Vz[i] * t_sp * z ** 2 / (2 * Ixx)
-        I2 = qb2(h)
-        s2 = np.arange(0, h+ 0.1, 0.1)
+#         # Base region 2
+#         qb2 = lambda z: -Vz[i] * t_sp * z ** 2 / (2 * Ixx)
+#         I2 = qb2(h)
+#         s2 = np.arange(0, h+ 0.1, 0.1)
 
-        # Base region 3
-        qb3 = lambda z: - Vz[i] * tarr[i] * (0.5 * h) * z / Ixx + I1 + I2
-        I3 = qb3(0.6 * c)
-        s3 = np.arange(0, 0.6*c+ 0.1, 0.1)
+#         # Base region 3
+#         qb3 = lambda z: - Vz[i] * tarr[i] * (0.5 * h) * z / Ixx + I1 + I2
+#         I3 = qb3(0.6 * c)
+#         s3 = np.arange(0, 0.6*c+ 0.1, 0.1)
 
-        # Base region 4
-        qb4 = lambda z: -Vz[i] * t_sp * z ** 2 / (2 * Ixx)
-        I4 = qb4(h)
-        s4=np.arange(0, h+ 0.1, 0.1)
+#         # Base region 4
+#         qb4 = lambda z: -Vz[i] * t_sp * z ** 2 / (2 * Ixx)
+#         I4 = qb4(h)
+#         s4=np.arange(0, h+ 0.1, 0.1)
 
-        # Base region 5
-        qb5 = lambda z: -Vz[i] * tarr[i] / Ixx * (0.5 * h * z - 0.5 * 0.5 * h * z ** 2 / l_sk) + I3 + I4
-        I5 = qb5(l_sk)
+#         # Base region 5
+#         qb5 = lambda z: -Vz[i] * tarr[i] / Ixx * (0.5 * h * z - 0.5 * 0.5 * h * z ** 2 / l_sk) + I3 + I4
+#         I5 = qb5(l_sk)
 
-        # Base region 6
-        qb6 = lambda z: Vz[i] * tarr[i] / Ixx * 0.5 * 0.5 * h / l_sk * z ** 2 + I5
-        I6 = qb6(l_sk)
+#         # Base region 6
+#         qb6 = lambda z: Vz[i] * tarr[i] / Ixx * 0.5 * 0.5 * h / l_sk * z ** 2 + I5
+#         I6 = qb6(l_sk)
 
-        # Base region 7
-        qb7 = lambda z: -Vz[i] * t_sp * 0.5 * z ** 2 / Ixx
-        I7 = qb7(-h)
-
-
-        # Base region 8
-        qb8 = lambda z: -Vz[i] * 0.5 * h * t_sp * z / Ixx + I6 - I7
-        I8 = qb8(0.6 * c)
-
-        # Base region 9
-        qb9 = lambda z: -Vz[i] * 0.5 * t_sp * z ** 2 / Ixx
-        I9 = qb9(-h)
-
-        # Base region 10
-        qb10 = lambda z: -Vz[i] * tarr[i] * (0.5 * h) ** 2 * (np.cos(z) - 1) / Ixx + I8 - I9
-
-        # Redundant shear flow
-        A11 = pi * (0.5 * h) / tarr[i] + h / t_sp
-        A12 = -h / t_sp
-        A21 = - h / t_sp
-        A22 = 1.2 * c / tarr[i]
-        A23 = -h / t_sp
-        A32 = - h / t_sp
-        A33 = 2 * l_sk / tarr[i] + h / t_sp
+#         # Base region 7
+#         qb7 = lambda z: -Vz[i] * t_sp * 0.5 * z ** 2 / Ixx
+#         I7 = qb7(-h)
 
 
+#         # Base region 8
+#         qb8 = lambda z: -Vz[i] * 0.5 * h * t_sp * z / Ixx + I6 - I7
+#         I8 = qb8(0.6 * c)
 
-        B1 = 0.5 * h / tarr[i] * trapz([qb1(0),qb1(pi/2)], [0, pi / 2]) + trapz([qb2(0),qb2(0.5*h)], [0, 0.5 * h]) / t_sp - trapz([qb9(-0.5*h),qb9(0)], [-0.5 * h, 0])/ t_sp + trapz([qb10(-pi/2),qb10(0)], [-pi / 2, 0]) * 0.5 * h / tarr[i]
-        B2 = trapz([qb2(0),qb2(0.5*h)], [0, 0.5 * h]) / t_sp + trapz([qb3(0),qb3(0.6*c)], [0, 0.6 * c]) / tarr[i] - trapz([qb7(-0.5*h),qb7(0)], [-0.5 * h, 0]) / t_sp + \
-             trapz([qb4(0),qb4(0.5*h)], [0, 0.5 * h]) / t_sp + trapz([qb8(0),qb8(0.6*c)], [0, 0.6 * c]) / tarr[i] - trapz([qb9(-0.5*h),qb9(0)], [-0.5 * h, 0]) / t_sp
-        B3 = trapz([qb5(0),qb5(l_sk)], [0, l_sk]) / tarr[i] + trapz([qb6(0),qb6(l_sk)], [0, l_sk]) / tarr[i] + trapz([qb4(0),qb4(0.5*h)], [0, 0.5 * h]) / t_sp - \
-             trapz([qb9(-0.5*h),qb9(0)], [-0.5 * h, 0]) / t_sp
+#         # Base region 9
+#         qb9 = lambda z: -Vz[i] * 0.5 * t_sp * z ** 2 / Ixx
+#         I9 = qb9(-h)
 
-        A = np.array([[A11, A12, 0], [A21, A22, A23], [0, A32, A33]])
-        B = -np.array([[B1], [B2], [B3]])
-        X = np.linalg.solve(A, B)
+#         # Base region 10
+#         qb10 = lambda z: -Vz[i] * tarr[i] * (0.5 * h) ** 2 * (np.cos(z) - 1) / Ixx + I8 - I9
 
-        q01 = float(X[0])
-        q02 = float(X[1])
-        q03 = float(X[2])
+#         # Redundant shear flow
+#         A11 = pi * (0.5 * h) / tarr[i] + h / t_sp
+#         A12 = -h / t_sp
+#         A21 = - h / t_sp
+#         A22 = 1.2 * c / tarr[i]
+#         A23 = -h / t_sp
+#         A32 = - h / t_sp
+#         A33 = 2 * l_sk / tarr[i] + h / t_sp
 
-        # Compute final shear flow
-        q2 = qb2(s2) - q01 + q02
-        q3 = qb3(s3) + q02
-        q4 = qb4(s4) + q03 - q02
 
-        max_region2 = max(q2)
-        max_region3 = max(q3)
-        max_region4 = max(q4)
-        determine = max(max_region2, max_region3, max_region4)
-        Nxy[i] = determine
-    return Nxy
-'''
+
+#         B1 = 0.5 * h / tarr[i] * trapz([qb1(0),qb1(pi/2)], [0, pi / 2]) + trapz([qb2(0),qb2(0.5*h)], [0, 0.5 * h]) / t_sp - trapz([qb9(-0.5*h),qb9(0)], [-0.5 * h, 0])/ t_sp + trapz([qb10(-pi/2),qb10(0)], [-pi / 2, 0]) * 0.5 * h / tarr[i]
+#         B2 = trapz([qb2(0),qb2(0.5*h)], [0, 0.5 * h]) / t_sp + trapz([qb3(0),qb3(0.6*c)], [0, 0.6 * c]) / tarr[i] - trapz([qb7(-0.5*h),qb7(0)], [-0.5 * h, 0]) / t_sp + \
+#              trapz([qb4(0),qb4(0.5*h)], [0, 0.5 * h]) / t_sp + trapz([qb8(0),qb8(0.6*c)], [0, 0.6 * c]) / tarr[i] - trapz([qb9(-0.5*h),qb9(0)], [-0.5 * h, 0]) / t_sp
+#         B3 = trapz([qb5(0),qb5(l_sk)], [0, l_sk]) / tarr[i] + trapz([qb6(0),qb6(l_sk)], [0, l_sk]) / tarr[i] + trapz([qb4(0),qb4(0.5*h)], [0, 0.5 * h]) / t_sp - \
+#              trapz([qb9(-0.5*h),qb9(0)], [-0.5 * h, 0]) / t_sp
+
+#         A = np.array([[A11, A12, 0], [A21, A22, A23], [0, A32, A33]])
+#         B = -np.array([[B1], [B2], [B3]])
+#         X = np.linalg.solve(A, B)
+
+#         q01 = float(X[0])
+#         q02 = float(X[1])
+#         q03 = float(X[2])
+
+#         # Compute final shear flow
+#         q2 = qb2(s2) - q01 + q02
+#         q3 = qb3(s3) + q02
+#         q4 = qb4(s4) + q03 - q02
+
+#         max_region2 = max(q2)
+#         max_region3 = max(q3)
+#         max_region4 = max(q4)
+#         determine = max(max_region2, max_region3, max_region4)
+#         Nxy[i] = determine
+#     return Nxy
 
 
 def local_buckling(c_r, b_st,t):
@@ -1104,45 +1102,45 @@ def WingboxOptimizer(x, wing, engine):
     # Initial values for the optimization 
 
     #Constants
-    prob.model.set_input_defaults('Integrated_design.b', wing.span)
-    prob.model.set_input_defaults('Integrated_design.c_r', wing.chord_root)
-    prob.model.set_input_defaults('Integrated_design.engine', engine)
-    prob.model.set_input_defaults('Integrated_design.wing', wing)
+    prob.model.set_input_defaults('wingbox_design.b', wing.span)
+    prob.model.set_input_defaults('wingbox_design.c_r', wing.chord_root)
+    prob.model.set_input_defaults('wingbox_design.engine', engine)
+    prob.model.set_input_defaults('wingbox_design.wing', wing)
 
     # Initial estimate for the design variables
-    prob.model.set_input_defaults('Integrated_design.tsp', x[0])
-    prob.model.set_input_defaults('Integrated_design.trib', x[1])
-    prob.model.set_input_defaults('Integrated_design.L', x[2])
-    prob.model.set_input_defaults('Integrated_design.bst', x[3])
-    prob.model.set_input_defaults('Integrated_design.hst', x[4])
-    prob.model.set_input_defaults('Integrated_design.tst', x[5])
-    prob.model.set_input_defaults('Integrated_design.wst', x[6])
-    prob.model.set_input_defaults('Integrated_design.t', x[7])
+    prob.model.set_input_defaults('wingbox_design.tsp', x[0])
+    prob.model.set_input_defaults('wingbox_design.trib', x[1])
+    prob.model.set_input_defaults('wingbox_design.L', x[2])
+    prob.model.set_input_defaults('wingbox_design.bst', x[3])
+    prob.model.set_input_defaults('wingbox_design.hst', x[4])
+    prob.model.set_input_defaults('wingbox_design.tst', x[5])
+    prob.model.set_input_defaults('wingbox_design.wst', x[6])
+    prob.model.set_input_defaults('wingbox_design.t', x[7])
 
 
     # Define constraints 
-    prob.model.add_constraint('Integrated_design.global_local', lower=0)
-    prob.model.add_constraint('Integrated_design.post_buckling', lower=0)
-    prob.model.add_constraint('Integrated_design.von_mises', lower=0)
-    prob.model.add_constraint('Integrated_design.buckling_constr', lower=0)
-    prob.model.add_constraint('Integrated_design.flange_loc_loc', lower=0)
-    prob.model.add_constraint('Integrated_design.local_column', lower=0)
-    prob.model.add_constraint('Integrated_design.crippling', lower=0)
-    prob.model.add_constraint('Integrated_design.web_flange', lower=0)
+    prob.model.add_constraint('wingbox_design.global_local', lower=0)
+    prob.model.add_constraint('wingbox_design.post_buckling', lower=0)
+    prob.model.add_constraint('wingbox_design.von_mises', lower=0)
+    prob.model.add_constraint('wingbox_design.buckling_constr', lower=0)
+    prob.model.add_constraint('wingbox_design.flange_loc_loc', lower=0)
+    prob.model.add_constraint('wingbox_design.local_column', lower=0)
+    prob.model.add_constraint('wingbox_design.crippling', lower=0)
+    prob.model.add_constraint('wingbox_design.web_flange', lower=0)
 
     prob.driver = om.ScipyOptimizeDriver()
     prob.driver.options['optimizer'] = 'COBYLA'
 
-    prob.model.add_design_var('Integrated_design.tsp', lower = 0, upper= 0.1)
-    prob.model.add_design_var('Integrated_design.trib', lower = 0, upper= 0.1)
-    prob.model.add_design_var('Integrated_design.L', lower = 0, upper= 2)
-    prob.model.add_design_var('Integrated_design.bst',  lower = 0, upper= 0.5)
-    prob.model.add_design_var('Integrated_design.hst', lower = 0. , upper= 0.4)
-    prob.model.add_design_var('Integrated_design.tst', lower = 0., upper= 0.1)
-    prob.model.add_design_var('Integrated_design.wst', lower = 0., upper= 0.4 )
-    prob.model.add_design_var('Integrated_design.t', lower = 0., upper= 0.1)
+    prob.model.add_design_var('wingbox_design.tsp', lower = 0, upper= 0.1)
+    prob.model.add_design_var('wingbox_design.trib', lower = 0, upper= 0.1)
+    prob.model.add_design_var('wingbox_design.L', lower = 0, upper= 2)
+    prob.model.add_design_var('wingbox_design.bst',  lower = 0, upper= 0.5)
+    prob.model.add_design_var('wingbox_design.hst', lower = 0. , upper= 0.4)
+    prob.model.add_design_var('wingbox_design.tst', lower = 0., upper= 0.1)
+    prob.model.add_design_var('wingbox_design.wst', lower = 0., upper= 0.4 )
+    prob.model.add_design_var('wingbox_design.t', lower = 0., upper= 0.1)
 
-    prob.model.add_objective('Integrated_design.wing_weight')
+    prob.model.add_objective('wingbox_design.wing_weight')
 
     prob.setup()
     prob.run_driver()
