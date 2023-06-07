0180bacaf596a32666b30fd93bbde4c4116f2a90
Steven-dR119 <78860420+Steven-dR119@users.noreply.github.com>
2023-06-07 13:16:03 +0200

    Revert "Updated wingbox_georgina"
    This reverts commit ffcf777589ff6661ef1947dde0a0c63a5064ce9b.

--- input/data_structures/engine.py
+++ input/data_structures/engine.py
@@ -25,7 +25,6 @@ class Engine:
     mass_pertotalengine: float = None
     x_rotor_loc: float = None
     y_rotor_loc: float = None
-    nacelle_w: float = None
 
 
     def load(self):
@@ -38,4 +37,3 @@ class Engine:
         self.mass_pertotalengine = self.totalmass/self.no_engines
         self.x_rotor_loc = data["x_rotor_loc"]
         self.y_rotor_loc = data["y_rotor_loc"]
-        self.nacelle_w = data["nacelle_w"]
