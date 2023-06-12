137f682567ade9858e2a0f4ab5948033a4f05113
Damien Keijzer <damien.keijzer@gmail.com>
2023-06-12 09:44:47 +0200

    chaged integration file

---
+++ scripts/convergence/integration.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+import os
+import json
+import sys
+import pathlib as pl
+
+sys.path.append(str(list(pl.Path(__file__).parents)[2]))
+os.chdir(str(list(pl.Path(__file__).parents)[2]))
+
+from input.data_structures import *
+
+
+# WingboxClass =  Wing()
