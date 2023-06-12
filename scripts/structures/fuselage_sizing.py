a3e7dac593cdbfe30ff375106dbdbc3281aef23f
WAlbers99 <78804185+WAlbers99@users.noreply.github.com>
2023-06-12 16:12:41 +0200

    Update fuselage_sizing.py

--- scripts/structures/fuselage_sizing.py
+++ scripts/structures/fuselage_sizing.py
@@ -16,63 +16,50 @@ import input.data_structures.GeneralConstants as const
 
 m = 2500
 V = 0.5
-#s, A = fl.simple_crash_box(m, 20, 3.15*10**6, 9.1)
-#h0 = 1.6 + s
 
-h0 = 1.8
-b0 = 1.6
-Beta = 0.5
-ARe = 2.75
-n = 2
-l_tank = np.linspace(1, 5, 40)
-l_fuelcell = 0.3
-l_cockpit = 2
-l_cabin = 2.5
-l_fcs = 2 * l_fuelcell
+def fuselage_calculations(aircraftmass: float, h2tankVolume: float):
+    #s, A = fl.simple_crash_box(m, 20, 3.15*10**6, 9.1)
+    #h0 = 1.6 + s
 
-# inputs
-# mass
-# plateau stress
-# yield stress (peak)
-# yield strain
-# densification strain
-# impact velocity
-# initial crashbox height
+    h0 = 1.8
+    b0 = 1.6
+    Beta = 0.5
+    ARe = 2
+    n = 2
+    l_tank = np.linspace(1, 5, 40)
+    l_fuelcell = 0.3
+    l_cockpit = 2
+    l_cabin = 2.5
+    l_fcs = 2 * l_fuelcell
 
+    l_tail, upsweep, bc, hc, hf, bf, AR, l_tank = fl.minimum_tail_length(h0, b0, Beta, V, l_tank, ARe, n)
+    r_tank = bc/(2*n)
 
-s_p, s_y, e_0, e_d, v0, s0 = 0.5*10**6, 1.2*10**6, 0.038, 0.9, 9.1, 0.5
-crash_box_height, crash_box_area = fl.crash_box_height_convergerence(s_p, s_y, e_0, e_d, v0, s0, m)
-h0 = 1.6 + crash_box_height
+    l_cyl = l_tank - 2*r_tank
 
-l_tail, upsweep, bc, hc, hf, bf, AR, l_tank = fl.minimum_tail_length(h0, b0, Beta, V, l_tank, ARe, n)
-r_tank = bc/(2*n)
+    V_tank = 4/3*np.pi*r_tank**3 + np.pi*r_tank**2*l_cyl
 
-l_cyl = l_tank - 2*r_tank
+    ''' 
+    print('Tank length: ', l_tank)
+    print("Tail: ", l_tail)
+    print("Upsweep: ", upsweep)
+    print("Tank radius: ", r_tank)
+    print("hc: ", hc)
+    print("bc: ", bc)
+    print("hf: ", hf)
+    print("bf: ", bf)
 
-V_tank = 4/3*np.pi*r_tank**3 + np.pi*r_tank**2*l_cyl
+    print("V_tank:", V_tank)
+    '''
+    l_fuselage = l_cockpit + l_cabin + l_fcs + l_tail
 
-"""
-print('Tank length: ', l_tank)
-print("Tail: ", l_tail)
-print("Upsweep: ", upsweep)
-print("Tank radius: ", r_tank)
-print("hc: ", hc)
-print("bc: ", bc)
-print("hf: ", hf)
-print("bf: ", bf)
-print("V_tank:", V_tank)
-"""
-
-l_inner_fuselage = l_cockpit + l_cabin + l_fcs + l_tail
-l_outer_fuselage = l_inner_fuselage + 0.2
+    return l_fuselage, upsweep,
 
 #things to return
-# l_inner_fuselage
-# l_outer_fuselage
+# l_fuselage
 # upsweep
 # l_tank
 # r_tank
 # bc, hc
 
-print(l_inner_fuselage)
 
