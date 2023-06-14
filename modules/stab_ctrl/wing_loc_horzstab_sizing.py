9f2c174af743aa530d5f02f4fc4b16177681a3ae
WAlbers99 <78804185+WAlbers99@users.noreply.github.com>
2023-06-14 10:26:20 +0200

    update

--- modules/stab_ctrl/wing_loc_horzstab_sizing.py
+++ modules/stab_ctrl/wing_loc_horzstab_sizing.py
@@ -170,7 +170,7 @@ def wing_location_horizontalstab_size(WingClass, FuseClass, HorTailClass, Aerocl
         CLah = CLahcalc(A_h, beta, eta, Lambdah2)
 
         m_stab, q_stab = stab_formula_coefs(CLah, CLaAh, depsda, l_h, MAC, Vh_V_2, x_ac_stab_bar, SM)
-        print(m_stab, q_stab)
+       # print(m_stab, q_stab)
 
         Cm_ac_flaps = -0.1825#From delta CL0
         Cm_ac_fuselage = cmac_fuselage_contr(b_f, l_f, h_f, CL0_approach, S, MAC, CLaAh)  # CLaAh for ctrl is different than for stab if cruise in compressible flow
@@ -200,6 +200,7 @@ def wing_location_horizontalstab_size(WingClass, FuseClass, HorTailClass, Aerocl
     WingClass.x_lewing = log_final[np.where(log_final[:,1] == np.min(log_final[:,1]))[0], 0:2][0][0] *FuseClass.length_fuselage - 0.24 * WingClass.chord_mac - WingClass.x_lemac
     HorTailClass.hortailsurf_wingsurf =  log_final[np.where(log_final[:,1] == np.min(log_final[:,1]))[0], 0:2][0][1]
     HorTailClass.surface = log_final[np.where(log_final[:,1] == np.min(log_final[:,1]))[0], 0:2][0][1] * WingClass.surface
+    print(HorTailClass.surface)
     #Update all values
     WingClass.dump()
     FuseClass.dump()
